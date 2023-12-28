8e270df67c9cafec514e236cc9350c1e27e210b9
Kalana Indula <kalanasmedia@outlook.com>
2023-12-28 18:44:53 +0530

    Supplier form was added

--- src/main/java/lk/ijse/stockmanagerev02/controller/DashboardForm.java
+++ src/main/java/lk/ijse/stockmanagerev02/controller/DashboardForm.java
@@ -4,11 +4,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class DashboardForm {
+    public AnchorPane rootNode;
+    public AnchorPane root;
+
     public void customerBtnClicked(ActionEvent actionEvent) throws IOException {
 
         Parent rootNode= FXMLLoader.load(this.getClass().getResource("/view/customer_form.fxml"));
@@ -22,5 +26,27 @@ public class DashboardForm {
         stage.centerOnScreen();
         stage.setTitle("Customer Manager");
         stage.show();
+    }
+
+    public void itemBtnClicked(ActionEvent actionEvent) throws IOException {
+        //Fetching the scene from fxml form
+       Parent rootNode= FXMLLoader.load(this.getClass().getResource("/view/item_form.fxml"));
+       //Creating a scene from fxml form
+       Scene scene=new Scene(rootNode);
+       //Setting the scene on dashboard window
+        Stage primaryStage=(Stage)this.rootNode.getScene().getWindow();
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Item Manager");
+    }
+
+    public void supplierBtnClicked(ActionEvent actionEvent) throws IOException {
+        //Fetch the scene from fxml form
+        Parent supplierScene=FXMLLoader.load(this.getClass().getResource("/view/supplier_form.fxml"));
+
+
+        //Get a pane from dashboard windoq
+        this.root.getChildren().clear();
+        //apply new scene to the pane
+        this.root.getChildren().add(supplierScene);
     }
 }
