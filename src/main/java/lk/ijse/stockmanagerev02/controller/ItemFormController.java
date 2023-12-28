d8d8af5abeab29f86e8be7c09014360bdf2023f7
Kalana Indula <kalanasmedia@outlook.com>
2023-12-28 19:28:40 +0530

    added back button in item form

--- src/main/java/lk/ijse/stockmanagerev02/controller/ItemFormController.java
+++ src/main/java/lk/ijse/stockmanagerev02/controller/ItemFormController.java
@@ -1,4 +1,23 @@
 package lk.ijse.stockmanagerev02.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ItemFormController {
+    public AnchorPane itemRoot;
+
+    public void backBtnClicked(ActionEvent actionEvent) throws IOException {
+        Parent dashBoardScene= FXMLLoader.load(this.getClass().getResource("/view/dashboard_form.fxml"));
+
+        Scene scene=new Scene(dashBoardScene);
+        Stage stage= (Stage) this.itemRoot.getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Dashboard Form");
+    }
 }
